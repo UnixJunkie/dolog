36e3d367a6d3ff2cabdb13f1b22b073f39ee15aa
Francois Berenger <berenger@riken.jp>
2013-03-27 13:54:02 +0900

    updated license header added signatures of functions that are visible by clients of the module

---
+++ log.mli
@@ -0,0 +1,40 @@
+(* Copyright (c) 2013, Zhang Initiative Research Unit,
+ * Advance Science Institute, RIKEN
+ * 2-1 Hirosawa, Wako, Saitama 351-0198, Japan
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. *)
+
+type log_level = FATAL | ERROR | WARN | INFO | DEBUG
+
+val int_of_level : log_level -> int
+val string_of_level : log_level -> string
+
+val set_log_level : log_level -> unit
+val set_output : out_channel -> unit
+
+val fatal : string Lazy.t -> unit
+val error : string Lazy.t -> unit
+val warn : string Lazy.t -> unit
+val info : string Lazy.t -> unit
+val debug : string Lazy.t -> unit
