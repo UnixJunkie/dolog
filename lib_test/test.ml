4d69291f678b5632546cc470e28f23db9eb280a5
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-12-19 21:11:16 +0000

    Add more tests

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -1,17 +1,26 @@
-Log.set_log_level Log.DEBUG;
+let () =
+  Log.set_log_level Log.DEBUG;
 
-Log.color_on();
+  Log.color_on();
 
-Log.fatal (lazy "Is it fatal?");
-Log.error (lazy "Or just an error?");
-Log.warn  (lazy "Maybe only a warning sign?");
-Log.info  (lazy "Is it informative at least?");
-Log.debug (lazy "What? It was an old debugging trace?!");
+  Log.fatal (lazy "Is it fatal?");
+  Log.error (lazy "Or just an error?");
+  Log.warn  (lazy "Maybe only a warning sign?");
+  Log.info  (lazy "Is it informative at least?");
+  Log.debug (lazy "What? It was an old debugging trace?!");
 
-Log.color_off();
+  Log.color_off();
 
-Log.fatal (lazy "Is it fatal?");
-Log.error (lazy "Or just an error?");
-Log.warn  (lazy "Maybe only a warning sign?");
-Log.info  (lazy "Is it informative at least?");
-Log.debug (lazy "What? It was an old debugging trace?!");
+  Log.fatal (lazy "Is it fatal?");
+  Log.error (lazy "Or just an error?");
+  Log.warn  (lazy "Maybe only a warning sign?");
+  Log.info  (lazy "Is it informative at least?");
+  Log.debug (lazy "What? It was an old debugging trace?!")
+
+module L1 = Log.Make(struct let section = "l1" end)
+module L2 = Log.Make(struct let section = "---l2---" end)
+
+let () =
+  Log.color_on();
+  L1.debug (lazy "haha");
+  L2.debug (lazy "hoho")
