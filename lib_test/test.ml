75c23ba704d6e54260238429da171e56a5d1f3e1
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-12-19 20:44:59 +0000

    Move lib and test files in subdirs

---
+++ lib_test/test.ml
@@ -0,0 +1,17 @@
+Log.set_log_level Log.DEBUG;
+
+Log.color_on();
+
+Log.fatal (lazy "Is it fatal?");
+Log.error (lazy "Or just an error?");
+Log.warn  (lazy "Maybe only a warning sign?");
+Log.info  (lazy "Is it informative at least?");
+Log.debug (lazy "What? It was an old debugging trace?!");
+
+Log.color_off();
+
+Log.fatal (lazy "Is it fatal?");
+Log.error (lazy "Or just an error?");
+Log.warn  (lazy "Maybe only a warning sign?");
+Log.info  (lazy "Is it informative at least?");
+Log.debug (lazy "What? It was an old debugging trace?!");
