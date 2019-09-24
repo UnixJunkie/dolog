323a0b51147254b7ceabf333ed285a4bc4ca5860
Francois Berenger <unixjunkie@sdf.org>
2019-09-24 14:30:00 +0900

    rm build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-\rm -f setup.*
-oasis setup
-./configure
-ocaml setup.ml -configure --prefix `opam conf var prefix`
-ocaml setup.ml -build
-ocamlfind remove dolog
-ocaml setup.ml -install
